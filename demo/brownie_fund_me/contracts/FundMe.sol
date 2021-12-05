77653d0dfbb1ac711565c870f513cf4f91e2abf7
shojir <octps4@gmail.com>
2021-12-05 22:19:29 +0900

    # On the Lesson6 ## section until with using chainlink-mix/contracts/test/MockV3Aggregator.sol for change price_feed_address and publish_source(verify flugs) according to networks.

---
+++ demo/brownie_fund_me/contracts/FundMe.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+// Interfaces compile down to an ABI(Application Binary Interface).
+// The ABI tells solidity and other programming languages how it
+// can interact with another contract.
+// Anytime you wnat to interact with an already deployed smart contract,
+// You will need an ABI.
+// Always need an ABI to interact with a contract.
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+// interface AggregatorV3Interface {
+//   function decimals() external view returns (uint8);
+//   function description() external view returns (string memory);
+//   function version() external view returns (uint256);
+// 
+//   getRoundData and latestRoundData should both raise "No data present"
+//   if they do not have data to report, instead of returning unset values
+//   which could be misinterpreted as actual reported values.
+//   function getRoundData(uint80 _roundId)
+    // external
+    // view
+    // returns (
+    //   uint80 roundId,
+    //   int256 answer,
+    //   uint256 startedAt,
+    //   uint256 updatedAt,
+    //   uint80 answeredInRound
+    // );
+// 
+//   function latestRoundData()
+    // external
+    // view
+    // returns (
+    //   uint80 roundId,
+    //   int256 answer,
+    //   uint256 startedAt,
+    //   uint256 updatedAt,
+    //   uint80 answeredInRound
+    // );
+// }
+// 
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
+contract FundMe {
+    using SafeMathChainlink for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address _priceFeed) public {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        owner = msg.sender;
+    }
+
+
+    function fund() public payable {
+        // $50
+        uint256 mimimumUSD = 50 * 10 ** 18;
+        // 1gwei < $50
+        require(getConversionRate(msg.value) >= mimimumUSD, "You need to spend more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
+        // what the ETH -> USD conversion rate
+    }
+
+    function getVersion() public view returns (uint256) {
+        return priceFeed.version();
+    }
+
+    // getPrice ETH/USD
+    function getPriceDefault() public view returns (uint256) {
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer); // answer(ETH) to (Gwei).
+    }
+
+    // getPrice ETH/USD (Gwei)
+    function getPrice() public view returns (uint256) {
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer * (10**10)); // answer(ETH) to (Gwei).
+    }
+
+    // 1Gwei = 1,000,000,000wei
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice(); // ETH/USD (Gwei)
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / (10**18);
+        return ethAmountInUsd;
+        // 4076770000000
+        // 0.000004076770000000 Gwei/USD
+    }
+}
---
+++ demo/brownie_fund_me/scripts/deploy.py
@@ -0,0 +1,29 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import get_account
+
+def deploy_fund_me():
+    account = get_account()
+    # pass the price feed address to our fundme contract
+    
+    # if we are on a persistent network like rinkeby, use the associated address
+    # otherwise, deploy mocks
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
+    else:
+        print(f"The active network is {network.show_active()}")
+        print("Deploying now...")
+        mock_aggregator = MockV3Aggregator.deploy(
+            18, 2*(1e+21), {"from": account}
+        )
+        price_feed_address = mock_aggregator.address
+        print("Mock Deployed!")
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print(f"Contract deployed to {fund_me.address}")
+
+def main():
+    deploy_fund_me()
