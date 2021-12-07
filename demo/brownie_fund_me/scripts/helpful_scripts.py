d529921b8205ee4a5f079068c2230ca056133c7f
shojir <octps4@gmail.com>
2021-12-07 23:29:37 +0900

    # Finished Lesson6 !!

--- demo/brownie_fund_me/scripts/helpful_scripts.py
+++ demo/brownie_fund_me/scripts/helpful_scripts.py
@@ -1,13 +1,17 @@
 from brownie import MockV3Aggregator, network, accounts, config
 from web3 import Web3
 
+FORKED_LOCAL_ENVIRNMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 DECIMALS = 8
 STARTING_PRICE = 2e+3*1e+8
 
 def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRNMENTS
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
