e3262bcce9e46693f71b42e9562e7009a2eaf6c6
shojir <octps4@gmail.com>
2021-12-07 22:57:10 +0900

    # on the lesson6 ## test finiched! 5:55:00

--- demo/brownie_fund_me/scripts/helpful_scripts.py
+++ demo/brownie_fund_me/scripts/helpful_scripts.py
@@ -4,7 +4,7 @@ from web3 import Web3
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 DECIMALS = 8
-STARTING_PRICE = 200000000000
+STARTING_PRICE = 2e+3*1e+8
 
 def get_account():
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
@@ -16,7 +16,5 @@ def deploy_mocks():
     print(f"The active network is {network.show_active()}")
     print("Deploying Mocks...")
     if len(MockV3Aggregator) <= 0:
-        mock_aggregator = MockV3Aggregator.deploy(
-            DECIMALS, STARTING_PRICE, {"from": get_account()}
-        )
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
     print("Mock Deployed!")
